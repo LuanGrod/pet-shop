e389e3f74f93c5d78ac78367f3de456d576de986
Rhenan Gomes <gomesrhenan@gmail.com>
2022-12-03 17:21:10 -0300

    Construção da página dinamica de produtos

--- client/src/components/Vitrine.jsx
+++ client/src/components/Vitrine.jsx
@@ -1,10 +1,36 @@
 import React from "react";
-import ProdutoBox from "../components/ProdutoBox"
+import { useEffect, useState } from "react";
 
 function Vitrine() {
+  const [produtos, setProdutos] = useState([]);
+ 
+    function buscaProdutos() {
+      fetch(`http://localhost:6969/produtos`)
+      .then(resposta => {
+          return resposta.json();
+      })
+      .then(resposta => {
+          const vetor = Object.values(resposta);
+          setProdutos(vetor);
+      });
+    }
+     
+    useEffect(() => {
+      buscaProdutos() 
+    }, [])
+
   return (
-    <aside className="w-3/5 h-auto ml-5 bg-brand-primary">
-      <ProdutoBox/>
+    <aside className="flex w-3/5 h-auto ml-5 bg-brand-primary">
+      {    
+      produtos.map(item => 
+          <div className="m-4 p-3">
+            <img src={item[2]} className="w-20 h-20"/>
+            <h1>{item[0]}</h1> 
+            <h1>{item[1]}</h1>
+            <button className="bg-slate-600">Adicionar ao carrinho</button>
+          </div>
+          )
+      }
     </aside>
   );
 }
